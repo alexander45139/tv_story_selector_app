ae91ddaf9aabf38125cd33e5d1fc8c2a7660f34b
alexander45139 <61944904+alexander45139@users.noreply.github.com>
2025-02-28 18:02:12 +0000

    Update 28/02/2025 18:02

--- src/HomeInterface/HomeInterfaceWindow.js
+++ src/HomeInterface/HomeInterfaceWindow.js
@@ -13,7 +13,7 @@ class HomeInterfaceWindow extends React.Component {
     componentDidMount() {
         fetch(`${sessionStorage.getItem('NodeAppDomain')}getAllSeries`, {method: 'GET'})
             .then(response => response.json().then(results => {
-                const allFetchedSeries: Series[] = results.series.map(se =>
+                const allFetchedSeries = results.series.map(se =>
                     new Series(se.TvMazeID, se.EpisodateID, se.ImdbID, se.Name, se.Premiered, se.Image, se.SeriesID)
                 );
                 this.setState({allSeries: allFetchedSeries});
